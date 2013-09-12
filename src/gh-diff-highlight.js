5cbbe0fda9b0820e989dceb1df402eefd5304cc9
Daniel Ribeiro <danielrb@pagerduty.com>
2013-09-12 00:58:36 -0700

    Preventing non breaking space from being treated as regular white text when translating them to the pre formated text. Fixes issue #1

--- src/gh-diff-highlight.js
+++ src/gh-diff-highlight.js
@@ -15,12 +15,16 @@ $.SyntaxHighlighter.load = function() {
     return this;
 }
 
+var escapeNbsp = function(str) { return str.replace(/&nbsp;/g, "\x11"); }
+var unEscapeNbsp = function(str) { return str.replace(/\x11/g, " "); }
+
 $(function() {
     $("pre.diff-line-pre").not('#discussion_bucket pre.diff-line-pre')
     var sel = function(s) { return $(s).not('#discussion_bucket ' + s); }
     sel(".diff-line-code").each(function() {
         var t = $(this);
-        var text = $.trim(t.text());
+        t.html(escapeNbsp(t.html()))
+        var text = unEscapeNbsp($.trim(t.text()));
         t.contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).remove()
         t.contents().filter(function() { return this.nodeName == "SPAN"; }).remove()
         t.append("<pre></pre>");
