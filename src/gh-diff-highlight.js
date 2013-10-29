bc9333f234d46a9a022377f805097139005a50e9
Daniel Ribeiro <danrbr@gmail.com>
2013-10-28 22:50:04 -0700

    no more need for escaping

--- src/gh-diff-highlight.js
+++ src/gh-diff-highlight.js
@@ -15,21 +15,7 @@ $.SyntaxHighlighter.load = function() {
     return this;
 }
 
-var escapeNbsp = function(str) { return str.replace(/&nbsp;/g, "\x11"); }
-var unEscapeNbsp = function(str) { return str.replace(/\x11/g, " "); }
-
 $(function() {
-    $("pre.diff-line-pre").not('#discussion_bucket pre.diff-line-pre')
-    var sel = function(s) { return $(s).not('#discussion_bucket ' + s); }
-    sel(".diff-line-code").each(function() {
-        var t = $(this);
-        t.html(escapeNbsp(t.html()))
-        var text = unEscapeNbsp($.trim(t.text()));
-        t.contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).remove()
-        t.contents().filter(function() { return this.nodeName == "SPAN"; }).remove()
-        t.append("<pre></pre>");
-        t.find("pre").text(text);
-    }) 
-    sel(".diff-line-code pre").addClass("highlight");
+    $(".diff-line-pre").addClass("highlight");
     $.SyntaxHighlighter.init({lineNumbers: false});
 });
